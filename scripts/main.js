f3f32d964d922239dac8869dbf771c3d683ab222
vit-tolokonnikov <vit.tolokonnikov@gmail.com>
2023-01-19 02:17:40 +0700

    Team activity was completed

--- scripts/main.js
+++ scripts/main.js
@@ -8,3 +8,16 @@ makeDarkButton.addEventListener("click", () => {
         body.classList.add('make-dark')
     }
 });
+
+const visitsDisplay = document.querySelector(".visits");
+let numVisits = Number(window.localStorage.getItem("visits"));
+
+if (numVisits !== 0) {
+	visitsDisplay.textContent = numVisits;
+} else {
+	visitsDisplay.textContent = `This is your first visit!`;
+}
+
+numVisits++;
+
+localStorage.setItem("visits", numVisits);
