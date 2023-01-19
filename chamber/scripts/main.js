89d7ac9812e33e963f6ba4007b9387e1a53d41f9
vit-tolokonnikov <vit.tolokonnikov@gmail.com>
2023-01-20 04:55:17 +0700

    The Chamber Discover Page

--- chamber/scripts/main.js
+++ chamber/scripts/main.js
@@ -15,3 +15,16 @@ hamMenu.addEventListener('click', () => {
         hamMenu.innerHTML = 'x';
     }
 }); 
+
+const visitsDisplay = document.querySelector(".visits");
+let numVisits = Number(window.localStorage.getItem("visits"));
+
+if (numVisits !== 0) {
+	visitsDisplay.textContent = numVisits;
+} else {
+	visitsDisplay.textContent = `This is your first visit!`;
+}
+
+numVisits++;
+
+localStorage.setItem("visits", numVisits);
